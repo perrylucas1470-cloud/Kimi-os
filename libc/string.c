b8e1e1ad43179cc0860ee3703d4d8559ac54133f
perrylucas1470-cloud <perrylucas1470@gmail.com>
2026-03-03 20:19:07 +0000

    Create string.c

---
+++ libc/string.c
@@ -0,0 +1,38 @@
+#include "string.h"
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while (str[len]) len++;
+    return len;
+}
+
+void* memset(void* ptr, int value, size_t num) {
+    uint8_t* p = (uint8_t*)ptr;
+    while (num--) {
+        *p++ = (uint8_t)value;
+    }
+    return ptr;
+}
+
+void* memcpy(void* dest, const void* src, size_t num) {
+    uint8_t* d = (uint8_t*)dest;
+    const uint8_t* s = (const uint8_t*)src;
+    while (num--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
+
+int strcmp(const char* str1, const char* str2) {
+    while (*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++;
+    }
+    return *(unsigned char*)str1 - *(unsigned char*)str2;
+}
+
+char* strcpy(char* dest, const char* src) {
+    char* d = dest;
+    while ((*d++ = *src++));
+    return dest;
+}
