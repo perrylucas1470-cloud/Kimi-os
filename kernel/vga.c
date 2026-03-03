7a13a4c8e8e03c6bd1618fd850485056a9afda35
perrylucas1470-cloud <perrylucas1470@gmail.com>
2026-03-03 20:40:32 +0000

    Update vga.c

--- kernel/vga.c
+++ kernel/vga.c
@@ -1,4 +1,6 @@
 #include "vga.h"
+#include "../libc/string.h"
+#include <stdarg.h>
 
 static uint16_t* vga_buffer = (uint16_t*)VGA_ADDRESS;
 static uint8_t vga_color = VGA_COLOR_LIGHT_GREY | (VGA_COLOR_BLACK << 4);
@@ -21,6 +23,7 @@ void vga_clear(void) {
     }
     cursor_x = 0;
     cursor_y = 0;
+    vga_update_cursor();
 }
 
 void vga_setcolor(uint8_t color) {
@@ -46,18 +49,10 @@ void vga_putchar(char c) {
     }
     
     if (cursor_y >= VGA_HEIGHT) {
-        // Scroll up
-        for (int y = 0; y < VGA_HEIGHT - 1; y++) {
-            for (int x = 0; x < VGA_WIDTH; x++) {
-                vga_buffer[y * VGA_WIDTH + x] = vga_buffer[(y + 1) * VGA_WIDTH + x];
-            }
-        }
-        // Clear last line
-        for (int x = 0; x < VGA_WIDTH; x++) {
-            vga_buffer[(VGA_HEIGHT - 1) * VGA_WIDTH + x] = vga_entry(' ', vga_color);
-        }
-        cursor_y = VGA_HEIGHT - 1;
+        vga_scroll();
     }
+    
+    vga_update_cursor();
 }
 
 void vga_puts(const char* str) {
@@ -66,8 +61,97 @@ void vga_puts(const char* str) {
     }
 }
 
-// Simple printf implementation
+void vga_scroll(void) {
+    // Move everything up one line
+    for (int y = 0; y < VGA_HEIGHT - 1; y++) {
+        for (int x = 0; x < VGA_WIDTH; x++) {
+            vga_buffer[y * VGA_WIDTH + x] = vga_buffer[(y + 1) * VGA_WIDTH + x];
+        }
+    }
+    // Clear last line
+    for (int x = 0; x < VGA_WIDTH; x++) {
+        vga_buffer[(VGA_HEIGHT - 1) * VGA_WIDTH + x] = vga_entry(' ', vga_color);
+    }
+    cursor_y = VGA_HEIGHT - 1;
+}
+
+void vga_update_cursor(void) {
+    uint16_t pos = cursor_y * VGA_WIDTH + cursor_x;
+    
+    outb(0x3D4, 14);
+    outb(0x3D5, (pos >> 8) & 0xFF);
+    outb(0x3D4, 15);
+    outb(0x3D5, pos & 0xFF);
+}
+
+// Simple number to string conversion
+static void itoa(int value, char* str, int base) {
+    char* ptr = str, *ptr1 = str, tmp_char;
+    int tmp_value;
+    
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz"[35 + (tmp_value - value * base)];
+    } while (value);
+    
+    // Apply negative sign
+    if (tmp_value < 0) *ptr++ = '-';
+    *ptr-- = '\0';
+    
+    // Reverse string
+    while (ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr-- = *ptr1;
+        *ptr1++ = tmp_char;
+    }
+}
+
 void vga_printf(const char* format, ...) {
-    // Simplified - just print the string for now
-    vga_puts(format);
+    va_list args;
+    va_start(args, format);
+    
+    char buffer[32];
+    char* str;
+    int num;
+    
+    while (*format) {
+        if (*format == '%' && *(format + 1)) {
+            format++;
+            switch (*format) {
+                case 's':
+                    str = va_arg(args, char*);
+                    vga_puts(str ? str : "(null)");
+                    break;
+                case 'd':
+                case 'i':
+                    num = va_arg(args, int);
+                    itoa(num, buffer, 10);
+                    vga_puts(buffer);
+                    break;
+                case 'x':
+                case 'X':
+                    num = va_arg(args, int);
+                    itoa(num, buffer, 16);
+                    vga_puts("0x");
+                    vga_puts(buffer);
+                    break;
+                case 'c':
+                    vga_putchar((char)va_arg(args, int));
+                    break;
+                case '%':
+                    vga_putchar('%');
+                    break;
+                default:
+                    vga_putchar('%');
+                    vga_putchar(*format);
+                    break;
+            }
+        } else {
+            vga_putchar(*format);
+        }
+        format++;
+    }
+    
+    va_end(args);
 }
