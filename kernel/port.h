6d9c92a688c7171cb7f42755ded64259d3352a8b
perrylucas1470-cloud <perrylucas1470@gmail.com>
2026-03-03 20:22:45 +0000

    Create port.h

---
+++ kernel/port.h
@@ -0,0 +1,27 @@
+#ifndef PORT_H
+#define PORT_H
+
+#include "../include/types.h"
+
+// Port I/O functions
+static inline uint8_t inb(uint16_t port) {
+    uint8_t result;
+    __asm__ volatile("inb %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+
+static inline void outb(uint16_t port, uint8_t data) {
+    __asm__ volatile("outb %0, %1" : : "a"(data), "Nd"(port));
+}
+
+static inline uint16_t inw(uint16_t port) {
+    uint16_t result;
+    __asm__ volatile("inw %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+
+static inline void outw(uint16_t port, uint16_t data) {
+    __asm__ volatile("outw %0, %1" : : "a"(data), "Nd"(port));
+}
+
+#endif
