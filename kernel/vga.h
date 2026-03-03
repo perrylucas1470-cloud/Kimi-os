c9814b71182ed5d86335b34861795502ca38afc9
perrylucas1470-cloud <perrylucas1470@gmail.com>
2026-03-03 21:23:50 +0000

    Update vga.h

--- kernel/vga.h
+++ kernel/vga.h
@@ -25,16 +25,8 @@
 #define VGA_COLOR_LIGHT_BROWN   14
 #define VGA_COLOR_WHITE         15
 
-// Port I/O for cursor
-static inline void outb(uint16_t port, uint8_t val) {
-    __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
-}
-
-static inline uint8_t inb(uint16_t port) {
-    uint8_t ret;
-    __asm__ volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
+// Port I/O functions - defined in port.h
+#include "port.h"
 
 void vga_init(void);
 void vga_clear(void);
