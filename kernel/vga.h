b6e3423db2a443bb528a2817578734986cc74f4d
perrylucas1470-cloud <perrylucas1470@gmail.com>
2026-03-03 20:14:06 +0000

    Create vga.h

---
+++ kernel/vga.h
@@ -0,0 +1,35 @@
+#ifndef VGA_H
+#define VGA_H
+
+#include "../include/types.h"
+
+#define VGA_ADDRESS 0xB8000
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+// Colors
+#define VGA_COLOR_BLACK         0
+#define VGA_COLOR_BLUE          1
+#define VGA_COLOR_GREEN         2
+#define VGA_COLOR_CYAN          3
+#define VGA_COLOR_RED           4
+#define VGA_COLOR_MAGENTA       5
+#define VGA_COLOR_BROWN         6
+#define VGA_COLOR_LIGHT_GREY    7
+#define VGA_COLOR_DARK_GREY     8
+#define VGA_COLOR_LIGHT_BLUE    9
+#define VGA_COLOR_LIGHT_GREEN   10
+#define VGA_COLOR_LIGHT_CYAN    11
+#define VGA_COLOR_LIGHT_RED     12
+#define VGA_COLOR_LIGHT_MAGENTA 13
+#define VGA_COLOR_LIGHT_BROWN   14
+#define VGA_COLOR_WHITE         15
+
+void vga_init(void);
+void vga_clear(void);
+void vga_putchar(char c);
+void vga_puts(const char* str);
+void vga_setcolor(uint8_t color);
+void vga_printf(const char* format, ...);
+
+#endif
